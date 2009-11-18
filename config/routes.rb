ffd462b3e6d0df10592e350a42ec2938d9d4c884
Matt McMahand <mmcmahand@gmail.com>
2009-11-18 18:09:56 -0500

    route updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
 
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "pages", :action => "home"
-  map.admin_root '/admin', :controller => 'admin/base', :action => 'index'
   # See how all your routes lay out with "rake routes"
+
+  map.root :controller => "pages", :action => "home"
+
   map.plans '/signup', :controller => 'accounts', :action => 'plans'
   map.thanks '/signup/thanks', :controller => 'accounts', :action => 'thanks'
   map.resource :account, :collection => { :dashboard => :get, :thanks => :get, :plans => :get, :billing => :any, :paypal => :any, :plan => :any, :cancel => :any, :canceled => :get }
@@ -41,6 +18,12 @@ ActionController::Routing::Routes.draw do |map|
     :member => { :profile => :any, :billing => :any,
       :billing_history => :get, :membership_terms => :get, :cancel_membership => :any
     }
+
+  # email confirmations
+  map.resources :users do |users|
+    users.resource :confirmation, :only => [:new, :create]
+  end
+
   map.resource :session
   map.resources :videos, :collection => { :search => :any }, :member => { :leave_suggestion => :post } do |videos|
     videos.resources :comments
@@ -49,7 +32,9 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :user_stories
   map.forgot_password '/forgot-password', :controller => 'sessions', :action => 'forgot'
   map.reset_password '/reset-password/:token', :controller => 'sessions', :action => 'reset'
+
   # admin-level stuff
+  map.admin_root '/admin', :controller => 'admin/base', :action => 'index'
 
   # Root-level routes.
   map.with_options :controller => 'sessions' do |sessions|
@@ -63,9 +48,11 @@ ActionController::Routing::Routes.draw do |map|
     sessions.formatted_sign_in '/sign-in.:format', :action => 'create',
       :conditions => { :method => :post }
   end
+
   map.with_options :controller => 'users' do |users|
     users.sign_up '/sign-up', :action => 'new'
   end
+
   # Routes for Pages
   map.with_options :controller => 'pages' do |pages|
     root_pages = ['about', 'advertising', 'contact', 'faqs', 'home',
@@ -76,8 +63,7 @@ ActionController::Routing::Routes.draw do |map|
       eval "pages.#{root_page.underscore} '/#{root_page}', :action => root_page.underscore"
     end
   end
-  # Da blog redirector
-  #map.connect '*blog', :controller => 'blog', :action => 'index'
+
   # Purchase-related stuff.
   map.cart '/cart', :controller => 'shopping_cart', :action => 'show'
   map.playlist '/playlist', :controller => 'playlists', :action => 'show'
@@ -85,6 +71,7 @@ ActionController::Routing::Routes.draw do |map|
   map.confirm_purchase '/confirm-purchase', :controller => 'shopping_cart', :action => 'confirm_purchase'
   map.purchase '/purchase/:id', :controller => 'purchases', :action => 'show'
   map.purchase_item '/purchase/:invoice_no/download/:id', :controller => 'purchases', :action => 'download'
+
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
