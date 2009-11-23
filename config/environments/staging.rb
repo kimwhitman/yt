ac6d9dd4e177e3baa8fa2b3713c77dd51b34eaaa
Matt McMahand <mmcmahand@gmail.com>
2009-11-23 12:33:26 -0500

    update action_mailer host

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -17,7 +17,7 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default_url_options = { :host => "gnsj4faa.joyent.us", :only_path => false }
+config.action_mailer.default_url_options = { :host => "staging.yogatoday.com", :only_path => false }
 
 # Delve Platform Values
 ENV['organization_id'] = '59b93524ab7c4d62b53d7553360c2b87'
