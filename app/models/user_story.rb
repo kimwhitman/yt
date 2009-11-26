6c7e52f9e629857ec7d36893cc2ce6eee1b455c7
Matt McMahand <mmcmahand@gmail.com>
2009-11-25 21:43:35 -0500

    better formatting

--- app/models/user_story.rb
+++ app/models/user_story.rb
@@ -5,8 +5,7 @@ class UserStory < ActiveRecord::Base
   validates_length_of :story, :maximum=> 3500
 
   named_scope :public, :conditions => { :is_public => true }
-  named_scope :by_publish_at,
-    :order => 'publish_at DESC'
+  named_scope :by_publish_at, :order => 'publish_at DESC'
   named_scope :published, :conditions => 'user_stories.publish_at <= NOW()'
   named_scope :unpublished, :conditions => 'user_stories.publish_at IS NULL'
   named_scope :publishable, :conditions => 'user_stories.publish_at IS NOT NULL'
