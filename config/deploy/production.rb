76436ca29e318903373b47db9420a2e568e8df7a
Matt McMahand <mmcmahand@gmail.com>
2009-12-30 16:36:28 -0500

    forgot to add these from last commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,18 +12,3 @@ role :db,  domain, :primary => true
 
 set :server_name, domain
 set :server_alias, domain
-
-deploy.task :restart do
-  # accelerator.smf_restart
-  accelerator.restart_apache
-end
-
-deploy.task :start do
-  # accelerator.smf_start
-  accelerator.restart_apache
-end
-
-deploy.task :stop do
-  # accelerator.smf_stop
-  accelerator.restart_apache
-end
