bed6ad4cc772ceb0ba2ce2e69200645ebc3e06ed
Matt McMahand <mmcmahand@gmail.com>
2010-01-01 15:55:19 -0500

    custom creditcard validation and cleaning

--- public/javascripts/billing.js
+++ public/javascripts/billing.js
@@ -4,11 +4,19 @@ var billing = {
 
       rules: {
         'creditcard[number]': {
+          checkcc: true,
           required:
-            function() {
-              return !$("#membership_free").is(':checked');
-            },
-          creditcard: true
+            function(element) {
+              free = $("#membership_free").is(':checked');
+              bogus = $("select#creditcard_type").val() == 'bogus';
+
+              if (free || (!free && bogus)) {
+                return false;
+              }
+              else {
+                return true;
+              }
+            }
         },
 
         'creditcard[verification_value]': {
@@ -45,5 +53,43 @@ var billing = {
 }
 
 $(document).ready(function() {
+
+  // custom cc validator that'll allow bogus types
+  $.validator.addMethod("checkcc", function(value, element) {
+      if($("select#creditcard_type").val() == 'bogus') {
+        return true;
+      }
+
+      if (/[^0-9-]+/.test(value)) {
+        return false;
+      }
+
+      var nCheck = 0,
+        nDigit = 0,
+        bEven = false;
+
+      value = value.replace(/\D/g, "");
+
+      for (n = value.length - 1; n >= 0; n--) {
+        var cDigit = value.charAt(n);
+        var nDigit = parseInt(cDigit, 10);
+        if (bEven) {
+          if ((nDigit *= 2) > 9)
+            nDigit -= 9;
+        }
+        nCheck += nDigit;
+        bEven = !bEven;
+      }
+
+      return (nCheck % 10) == 0;
+
+    },"Must be a valid credit card number.");
+
   billing.setup();
+
+  // remove spaces and dashes
+  $("#creditcard_number").change(function(){
+    $(this).val(($(this).val().replace(/[-\s]/g, '')));
+  });
 });
+
