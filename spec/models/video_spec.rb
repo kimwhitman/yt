8270768e6d8d4498d329865b9d9ff2695b8aa965
Carlos Rodriguez <carlos@eddorre.com>
2010-03-01 18:33:40 -0800

    Scoping show, preview, info, and related videos to published scope. Changed name of public scope to published.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,34 +1,30 @@
 require 'spec_helper'
 
 describe Video do
-  describe "Public Videos" do
+  describe "Published Videos" do
     after(:each) do
       Timecop.return
     end
         
-    it "should be public if is_public is true and the published_date is in the past" do
+    it "should be published if is_public is true and the published_date is in the past" do
       video = Video.create(:title => 'Test', :duration => 100, :description => 'This is a test', 
         :streaming_media_id => 1, :is_public => true, :published_at => Time.now)
-        
-      RAILS_DEFAULT_LOGGER.info("DEBUG #{Video.public.inspect}")
-        
       Timecop.freeze(Date.today + 2)
-      
-      Video.public.should include(video)
+      Video.published.should include(video)
     end
     
-    it "should not be public if is_public is false and the published_date is in the past" do
+    it "should not be published if is_public is false and the published_date is in the past" do
       video = Video.create(:title => 'Test', :duration => 100, :description => 'This is a test', 
         :streaming_media_id => 1, :is_public => false, :published_at => Time.now)
         
-        Video.public.should_not include(video)
+        Video.published.should_not include(video)
     end
     
-    it "should not be public if is_public is true but the published_date is in the future" do
+    it "should not be published if is_public is true but the published_date is in the future" do
       video = Video.create(:title => 'Test', :duration => 100, :description => 'This is a test', 
         :streaming_media_id => 1, :is_public => true, :published_at => Time.now.advance(:months => 3))
         
-      Video.public.should_not include(video)
+      Video.published.should_not include(video)
     end
   end
 end
