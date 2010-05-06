03b484dc3a6ef9366ce9a4e623b9fea8aeaaa8c4
Carlos Rodriguez <carlos@planetargon.com>
2010-05-06 14:16:14 -0700

    Extended token generator to generate simple non-guid tokens

--- lib/token_generator.rb
+++ lib/token_generator.rb
@@ -8,4 +8,27 @@ module TokenGenerator
   def set_token
     self.token = Guid.new.to_s
   end
+  
+  module Simple
+    TOKEN_LENGTH = 8
+    
+    def generate_token(length = TOKEN_LENGTH)
+      if (temp_token = random_token(length)) and self.class.find_by_token(temp_token).nil?
+        self.token = temp_token
+      else
+        generate_token
+      end
+    end
+    
+    def random_token(length = TOKEN_LENGTH)
+      characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+      temp_token = ''
+      srand
+      length.times do
+        pos = rand(characters.length)
+        temp_token += characters[pos..pos]
+      end
+      temp_token
+    end
+  end
 end
