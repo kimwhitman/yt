01c0bba77b95aeb74ac44cd9c6d00cac4fbc516b
Carlos Rodriguez <carlos@planetargon.com>
2010-05-07 11:06:14 -0700

    Excluding loading associations for update action as well

--- app/controllers/admin/share_urls_controller.rb
+++ app/controllers/admin/share_urls_controller.rb
@@ -2,5 +2,7 @@ class Admin::ShareUrlsController < Admin::BaseController
   active_scaffold :share_urls do |config|
     config.create.columns.exclude :user
     config.create.columns.exclude :share_url_redirects
+    config.update.columns.exclude :user
+    config.update.columns.exclude :share_url_redirects    
   end
 end
