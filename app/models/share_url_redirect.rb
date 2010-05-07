514733b74c3af24cc15cfac12086d593a773ff0a
Carlos Rodriguez <carlos@planetargon.com>
2010-05-06 18:09:46 -0700

    Fixed associations

--- app/models/share_url_redirect.rb
+++ app/models/share_url_redirect.rb
@@ -1,7 +1,7 @@
 class ShareUrlRedirect < ActiveRecord::Base
   # Associations
-  belongs_to :person
-  belongs_to :short_url
+  belongs_to :user
+  belongs_to :share_url
 
   # Validations
   validates_presence_of :short_url_id, :message => "can't be blank"
