58e58c3c188d7ccc24792a7beaec0d099b9c8bd1
Gary Blessington <garyblessington@gmail.com>
2010-05-24 12:55:20 -0700

    Added flash message for after an email invite is sent

--- app/controllers/ambassador_invites_controller.rb
+++ app/controllers/ambassador_invites_controller.rb
@@ -4,6 +4,7 @@ class AmbassadorInvitesController < ApplicationController
     @ambassador_invite = current_user.ambassador_invites.new(params[:ambassador_invite])
     if @ambassador_invite.save
       @ambassador_invite.deliver!
+      flash[:success] = "Thanks for sharing Yoga Today. We'll let you know right away when you've earned points for free yoga. <a href=\"#{ ambassador_tools_invite_by_email_user_path(self.current_user) }\">Send another invitation</a>"
       redirect_to ambassador_tools_my_invitations_user_path(current_user)
     else
       render :template => 'users/ambassador_tools/invite_by_email'
