043a14cb5385a7a8abec51f2ed23929539809207
Gary Blessington <garyblessington@gmail.com>
2010-07-14 15:57:47 -0700

    Asset hosts change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ ActionController::Base.asset_host = Proc.new { |source, request|
   if request && request.ssl?
     "#{request.protocol}#{request.host_with_port}"
   else
-    "http://assets%d.#{ HOST }"
+    "http://assets#{ rand(4) + 1 }.#{ HOST }"
   end
 }
 
