3a193cdef96686e61840904d957618b367777eed
Gary Blessington <garyblessington@gmail.com>
2010-07-27 11:27:50 -0700

    Added cancelled_at to users table in admin

--- db/migrate/20100727180433_add_cancelled_on_to_subscriptions.rb
+++ db/migrate/20100727180433_add_cancelled_on_to_subscriptions.rb
@@ -1,6 +1,7 @@
 class AddCancelledOnToSubscriptions < ActiveRecord::Migration
   def self.up
     add_column :subscriptions, :cancelled_at, :datetime
+    add_index :subscriptions, :cancelled_at
   end
 
   def self.down
