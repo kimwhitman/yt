ef1d2b0e1b0c99aa0113b3f7bdebf3ae7b218b22
Carlos Rodriguez <carlos@planetargon.com>
2010-10-26 15:25:32 -0700

    Moving destructive compact onto it's own line

--- app/models/video.rb
+++ app/models/video.rb
@@ -238,7 +238,8 @@ class Video < ActiveRecord::Base
         video.yoga_types << yoga_types unless yoga_types.blank?
 
         video_focuses.reject! { |video_focus| !video.video_focus.blank? && video.video_focus.include?(video_focus) }
-        video.video_focus << video_focuses.compact! unless video_focuses.blank?
+        video_focuses.compact!
+        video.video_focus <<  video_focuses unless video_focuses.blank?
 
         video.skill_level = skill_level
 
