5a14b6418449c6eb935811724dfb6e8cf6497ebd
verny <vriverad@gmail.com>
2011-06-21 11:47:41 -0600

    fixing an issue with paperclip

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,5 +1,7 @@
 class Instructor < ActiveRecord::Base
-  has_attached_file :photo
+  has_attached_file :photo,
+    :url => "/system/:attachment/:id/:style/:basename.:extension",
+   :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
   has_and_belongs_to_many :videos
   validates_presence_of :name
 end
